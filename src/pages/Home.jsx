0ead445f839c593ee688c75a00a7edcac7ded843
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-08-20 19:03:55 +0200

    Added Reducer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+import UsetSearch from '../components/users/UsetSearch'
+
+function Home() {
+  return (
+    <>
+      <UsetSearch />
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
